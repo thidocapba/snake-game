3100a3cd64457d2ae36d232ebb8c4869a493f5af
Dung Lo <lodung842001@gmail.com>
2021-12-05 01:02:57 +0700

    Second Commit

--- snakegame.py
+++ snakegame.py
@@ -37,3 +37,57 @@ score_msg_size = score_font.size("Score")
 background_color = pygame.Color(0, 0 , 0)   # we will fill background color as black
 black = pygame.Color(0, 0 ,0)
 
+
+# for clock at the left corner
+gameClock = pygame.time.Clock()
+
+
+# we will define keys
+
+def getKey():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                return KEY["UP"]
+            elif event.key == pygame.K_DOWN:
+                return KEY["DOWN"]
+            elif event.key == pygame.K_LEFT:
+                return KEY["LEFT"]
+            elif event.key == pygame.K_RIGHT:
+                return KEY["RIGHT"]
+            # for exit
+            elif event.key == pygame.K_ESCAPE:
+                return "exit"
+            # if we want to continue playing again
+            elif event.key == pygame.K_y:
+                return "yes"
+            # if we don't want to play game
+            elif event.key == pygame.K_n:
+                return "no"
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+
+def endGame():
+    message = game_over_font.render("Game Over", 1, pygame.Color("white"))
+    message_play_again = play_again_font.render("Play Again ? (Y/N)", 1, pygame.Color("green"))
+    screen.blit(message, (320, 240))
+    screen.blit(message_play_again, (320 + 12, 240 + 40))
+
+    pygame.display.flip()
+    pygame.display.update()
+
+    mKey = getKey()
+    while(mKey != "exit"):
+        if(mKey == "yes"):
+            main()
+        elif(mKey == "no"):
+            break
+        mKey = getKey()
+        gameClock.tick(FPS)
+    sys.exit(0)
+
+def drawScore(score):
+    score_numb = score_numb_font.render(str(score), 1, pygame.Color("red"))
+    screen.blit(score_msg, (SCREEN_WIDTH - score_msg_size[0] - 60, 10))
+    screen.blit(score_numb, (SCREEN_WIDTH - 45, 14))
+
